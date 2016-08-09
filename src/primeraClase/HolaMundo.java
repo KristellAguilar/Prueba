77f99105965bca709181d5f4d384513ed7ef0e0b
KristellAguilar <kristell153@gmail.com>
2016-08-09 01:55:22 -0400

    Commit segundo

--- src/primeraClase/HolaMundo.java
+++ src/primeraClase/HolaMundo.java
@@ -4,6 +4,12 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		int numero1 = 4;
+		int numero2 = 7;
+		int suma = 0;
+		suma = numero1 + numero2;
+		
+		System.out.println(suma);
 
 	}
 
